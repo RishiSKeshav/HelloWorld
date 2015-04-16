d6585ac9ebc10651eb075875056f12b7cac69208
RishiSKeshav <rishiskeshav91@gmail.com>
2015-04-16 16:12:08 -0700

    Hello World Inital Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//print out your name and github username
+        
+	       System.out.println("Hello World");
+
+	}
+	
+}
